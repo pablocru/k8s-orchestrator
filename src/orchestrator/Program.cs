aed96ccf42472fc1d9e1cdfad38f141a082bf8ae
pablocru <pablocru@outlook.com>
2025-10-31 12:51:42 +0100

    feat: Use Serilog to improve logging
    - Install Serilog
    - Configure globally tu log Info level
    - Configure in Production to log only custom messages and warning level

--- src/orchestrator/Program.cs
+++ src/orchestrator/Program.cs
@@ -1,5 +1,9 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
+
 var app = builder.Build();
 app.MapControllers();
 app.Run();
