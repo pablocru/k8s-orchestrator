9b13da621b9a887faafeecda890ca94559763018
pablocru <pablocru@outlook.com>
2025-11-03 11:18:26 +0100

    feat: Add ExampleTask for testing cron job

---
+++ src/orchestrator/Services/ExampleTask.cs
@@ -0,0 +1,17 @@
+﻿namespace Orchestrator.Services;
+
+public class ExampleTask : ICronJobTask
+{
+  private readonly ILogger<ExampleTask> _logger;
+
+  public ExampleTask(ILogger<ExampleTask> logger)
+  {
+    _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+  }
+
+  public async Task ExecuteAsync(CancellationToken cancellationToken)
+  {
+    _logger.LogInformation("Running task at {Time}.", DateTime.Now);
+    await Task.Delay(2000, cancellationToken);
+  }
+}
